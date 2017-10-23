a9cdbca272b71106a5578b363eb252154b622d4d
Israel <arnulfo_israel@hotmail.com>
2017-10-23 18:10:06 -0500

    Added dateUTC and boolean filter tests

---
+++ test/Unosquare.Tubular.Tests/ApiResultTest.cs
@@ -0,0 +1,44 @@
+﻿namespace Unosquare.Tubular.Tests
+{
+    using NUnit.Framework;
+    using Unosquare.Tubular.GenericModels;
+
+    [TestFixture]
+    class ApiResultTest
+    {
+        [Test]
+        public void AffectedCountTests()
+        {
+            var api = new ApiResult();
+            var affectedCount = 1000;
+
+            api.AffectedCount = affectedCount;
+
+            Assert.AreEqual(affectedCount, api.AffectedCount);
+        }
+
+        [Test]
+        public void OKTes()
+        {
+            var apiOK = new ApiResult();
+            var message = "This is a OK message";
+
+            var result = ApiResult.Ok(message);
+
+            Assert.AreEqual("OK", result.Status);
+            Assert.AreEqual(message, result.Message);
+        }
+
+        [Test]
+        public void ErrorTes()
+        {
+            var apiOK = new ApiResult();
+            var ex = new System.Exception("This is a Error message");
+
+            var result = ApiResult.Error(ex);
+
+            Assert.AreEqual("Error", result.Status);
+            Assert.AreEqual(ex.Message, result.Message);
+        }
+    }
+}
